356535cd64634e924b93b5c247a1218793c1fd1d
burkej1 <burkej1@student.unimelb.edu.au>
2018-02-14 10:28:31 +1100

    Split functions into new file

---
+++ amplicon_coverage_functions.R
@@ -0,0 +1,79 @@
+# Functions used in amplicon coverage plotting
+
+# Function to restructure a dataframe for heatmap plotting (note: will throw an error with a single sample)
+to_heatmap_matrix <- function(df, replace = FALSE, highvalue = 1000, lowvalue = 0) {
+  df_restructure <- dcast(df, amplicon_f~sample, fill=0)
+  fix_rownames <- df_restructure[,-1]
+  rownames(fix_rownames) <- df_restructure[,1]
+  if (replace == TRUE) {
+    fix_rownames <- replace(fix_rownames, fix_rownames > highvalue, highvalue)
+    fix_rownames <- replace(fix_rownames, fix_rownames < lowvalue, lowvalue)
+  }
+  return(data.matrix(fix_rownames))
+}
+
+
+# Labels values in a dataframe (for the overlayed dataframes)
+label_values <- function(x, value) {
+  gsub("^", paste(value, ": ", sep=""), x)
+}
+
+# Replaces all values in rows/columns with the output from a function called on those rows/columns
+hoverframe_metrics <- function(dm, function_name, by = "row") {
+  FUN <- match.fun(function_name)
+  if (by == "column") {
+    # Iterate through columns
+    for (n in 1:length(colnames(dm))) {
+      dm[,n] <- FUN(dm[,n])  # Replacing each column with the function output
+    }
+  } else if (by == "row") {
+    for (n in 1:length(row.names(dm))) {
+      dm[n,] <- FUN(dm[n,])
+    }
+  } else {
+    print("Unknown argument passed for 'by = '")
+  }
+  return(dm)
+}
+
+normalize_matrix <- function(dm, by = "amplicon") {
+  if (by == "amplicon") {
+    for (n in 1:length(row.names(dm))) {
+      rowmean <- mean(dm[n,])
+      normalized_row <- sapply(dm[n,], function(x, m) x / m, rowmean)
+      dm[n,] <- normalized_row
+    }
+  } else if (by == "sample") {
+    for (n in 1:length(colnames(dm))) {
+      colmean <- mean(dm[,n])
+      normalized_col <- sapply(dm[,n], function(x, m) x / m, colmean)
+      dm[,n] <- normalized_col
+    }
+  } else {
+    print("Invalid option")
+  }
+  return(dm)
+}
+
+# dm <- plotdata_all  # Testing
+# Creates dataframe with extra information for the heatmap plots
+generate_hoverframe <- function(dm) {
+  rowmeans <- sapply(data.frame(hoverframe_metrics(dm, "mean", by = "row")), label_values, "Amplicon Mean")
+  rowsd <- sapply(data.frame(hoverframe_metrics(dm, "sd", by = "row")), label_values, "Amplicon SD")
+  colmeans <- sapply(data.frame(hoverframe_metrics(dm, "mean", by = "column")), label_values, "Sample Mean")
+  colsd <- sapply(data.frame(hoverframe_metrics(dm, "sd", by = "column")), label_values, "Sample SD")
+  original_depths <- sapply(data.frame(dm), label_values, "Amplicon Depth")
+  
+  if (exists("new_df")) {
+    rm("new_df")
+  }
+  for (n in 1:length(colnames(dm))) {
+    newcolumn <- data.frame(paste(original_depths[, n], colmeans[, n], colsd[, n], rowmeans[, n], rowsd[, n], sep="\n"))
+    if (exists("new_df")) {
+      new_df <- cbind(new_df, newcolumn)
+    } else {
+      new_df <- data.frame(newcolumn)
+    }
+  }
+  return(new_df)
+}
--- plot_amplicon_coverage.R
+++ plot_amplicon_coverage.R
@@ -1,69 +1,11 @@
-library(ggplot2)
-library(readr)
-library(gplots)
-library(RColorBrewer) 
-library(reshape2) 
-library(heatmaply)
+library(ggplot2)  # General dependency for heatmaply/shinyheatmaply
+library(readr)  # For importing tsvs
+library(gplots)  # Used for something
+library(RColorBrewer)  # Colour palettes
+library(reshape2)  # For manipulating dataframes/matrices I think
+library(heatmaply)  # Interactive heatmaps
+library(scales)  # Rescaling scales
 
-
-# Function to restructure a dataframe for heatmap plotting (note: will throw an error with a single sample)
-to_heatmap_matrix <- function(df, replace = FALSE) {
-  df_restructure <- dcast(df, amplicon_f~sample, fill=0)
-  fix_rownames <- df_restructure[,-1]
-  rownames(fix_rownames) <- df_restructure[,1]
-  if (replace == TRUE) {
-    fix_rownames <- replace(fix_rownames, fix_rownames > 1000, 1000)
-  }
-  return(data.matrix(fix_rownames))
-}
-
-
-# Labels values in a dataframe (for the overlayed dataframes)
-label_values <- function(x, value) {
-  gsub("^", paste(value, ": ", sep=""), x)
-}
-
-# Replaces all values in rows/columns with the output from a function called on those rows/columns
-hoverframe_metrics <- function(dm, function_name, by = "row") {
-  FUN <- match.fun(function_name)
-  if (by == "column") {
-    # Iterate through columns
-    for (n in 1:length(colnames(dm))) {
-      dm[,n] <- FUN(dm[,n])  # Replacing each column with the function output
-    }
-  } else if (by == "row") {
-    for (n in 1:length(row.names(dm))) {
-      dm[n,] <- FUN(dm[n,])
-    }
-  } else {
-    print("Unknown argument passed for 'by = '")
-  }
-  return(dm)
-}
-
-
-# dm <- plotdata_all  # Testing
-# Creates dataframe with extra information for the heatmap plots
-generate_hoverframe <- function(dm) {
-  rowmeans <- sapply(data.frame(hoverframe_metrics(dm, "mean", by = "row")), label_values, "Amplicon Mean")
-  rowsd <- sapply(data.frame(hoverframe_metrics(dm, "sd", by = "row")), label_values, "Amplicon SD")
-  colmeans <- sapply(data.frame(hoverframe_metrics(dm, "mean", by = "column")), label_values, "Sample Mean")
-  colsd <- sapply(data.frame(hoverframe_metrics(dm, "sd", by = "column")), label_values, "Sample SD")
-  original_depths <- sapply(data.frame(dm), label_values, "Amplicon Depth")
-  
-  if (exists("new_df")) {
-    rm("new_df")
-  }
-  for (n in 1:length(colnames(dm))) {
-    newcolumn <- data.frame(paste(original_depths[, n], colmeans[, n], colsd[, n], rowmeans[, n], rowsd[, n], sep="\n"))
-    if (exists("new_df")) {
-      new_df <- cbind(new_df, newcolumn)
-    } else {
-      new_df <- data.frame(newcolumn)
-    }
-  }
-  return(new_df)
-}
 
 # Input file (should write to take a command line input)
 amplicon_metrics <- read_delim("/Users/Jared/Documents/Code/amplicon-coverage-plots/run1-7_amplicon_metrics_all.tsv", 
@@ -76,34 +18,33 @@ plotdata <- amplicon_metrics[,c("amplicon_f", "sample", "reads")]  # Subset data
 # plotdata <- plotdata[1:2386,]  # Subsetting for testing purposes
 
 # Creating matrices
-plotdata_all_capped <- to_heatmap_matrix(plotdata, TRUE)  # All samples and all amplicons, capped for visualisation
+plotdata_all_capped <- to_heatmap_matrix(plotdata, TRUE, lowvalue = 100, highvalue = 1000)  # All samples and all amplicons, capped for visualisation
 plotdata_all <- to_heatmap_matrix(plotdata, FALSE)  # Uncapped amplicon data, use this for overlay matrices
+full_hovertext <- generate_hoverframe(plotdata_all)  # Overlay matrix
 
-# Generating overlay matrix
-full_hovertext <- generate_hoverframe(plotdata_all)
-
-allplot <- heatmaply(plotdata_all_capped, 
-                     fontsize_col = 8, 
-                     fontsize_row = 8, 
-                     file = "all_coverage.html", 
-                     custom_hovertext = full_hovertext)
+all_heatmap <- heatmaply(plotdata_all, 
+                         fontsize_col = 8, 
+                         fontsize_row = 8, 
+                         file = "all_coverage.html", 
+                         custom_hovertext = full_hovertext)
 
 
 # Extract the gene names from the amplicons and create a heatmap for each gene
 gene_names <- unique(gsub("_.+_.+", "", plotdata$amplicon_f))
+gene = "PMS2"
 for (gene in gene_names) {
   main_frame <- plotdata[grep(gene, plotdata$amplicon_f),]
   gene_subset_capped <- to_heatmap_matrix(main_frame, TRUE)
   gene_subset <- to_heatmap_matrix(main_frame, FALSE)
   hovertext <- generate_hoverframe(gene_subset)
   plotname <- paste(gene, "_coverage.html", sep="")
-  geneplot <- heatmaply(gene_subset_capped, 
+  geneplot <- heatmaply(normalize(gene_subset), 
                         fontsize_col = 8, 
                         fontsize_row = 8, 
+                        margins = c(200, 150), 
                         custom_hovertext = hovertext, 
                         file = plotname)
 }
-
 
 
 
@@ -117,5 +58,16 @@ for (gene in gene_names) {
 #           )
 
 
-
+# plotdata_all_amplicon_norm <- normalize_matrix(plotdata_all, by = "sample")
+# plotdata_all_sample_norm <- normalize_matrix(plotdata_all, by = "amplicon")
+# 
+# colourbreaks <- rescale(c(0, 0.5, 1, 2, 10), to = c(0, 1))
+# colourscale <- brewer.pal(5, "Blues")
+# all_heatmap <- heatmaply(plotdata_all_amplicon_norm,
+#                          fontsize_col = 8, 
+#                          fontsize_row = 8, 
+#                          scale_fill_gradient_fun = scale_fill_gradientn(colors = colourscale,
+#                          values = colourbreaks),
+#                          file = "all_coverage_amplicon_norm.html", 
+#                          custom_hovertext = full_hovertext)
 
