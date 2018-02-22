cd04dffd20ad55cbe4f4a408dd8c953c6057ed7e
burkej1 <burkej1@student.unimelb.edu.au>
2018-02-22 15:34:20 +1100

    Add single sample plotting

--- create_index_html.sh
+++ create_index_html.sh
@@ -1,17 +1,16 @@
 # Takes an output file as first argument
 BASENAME=${1}
+DIRECTORY=${2%/}
 
-SUFFIXARRAY=( "_sample-normalised_coverage.html" "_amplicon-normalised_coverage.html" "_all.html" )
-
-# Creating these separately due to regex matching too much
-DEPTHPLOTS=$(ls coverage_plots/* | grep _coverage.html | grep -ve sample-normalised_coverage -ve amplicon-normalised_coverage)
+DEPTHPLOTS=$(ls ${DIRECTORY}/*)
 DEPTHOUTPUT="${BASENAME}_coverage.html"
+
 echo ${DEPTHOUTPUT}
 # Top bit of the html
 echo "<!DOCTYPE html>" > ${DEPTHOUTPUT}
 echo "<html>" >> ${DEPTHOUTPUT}
 echo "<body>" >> ${DEPTHOUTPUT}
-# Add iframes for each plot assuming plots are in a folder called "coverage_plots"
+# Add iframes for each plot
 for HTML in ${DEPTHPLOTS[@]}; do
     echo "<iframe src='${HTML}' style='border:none;' width='100%' height='1000'>" >> ${DEPTHOUTPUT}
     echo "</iframe>" >> ${DEPTHOUTPUT}
@@ -20,18 +19,22 @@ done
 echo "</body>" >> ${DEPTHOUTPUT}
 echo "</html>" >> ${DEPTHOUTPUT}
 
-for SUFFIX in ${SUFFIXARRAY[@]}; do
-    echo ${BASENAME}${SUFFIX}
-    # Top bit of the html
-    echo "<!DOCTYPE html>" > ${BASENAME}${SUFFIX}
-    echo "<html>" >> ${BASENAME}${SUFFIX}
-    echo "<body>" >> ${BASENAME}${SUFFIX}
-    # Add iframes for each plot assuming plots are in a folder called "coverage_plots"
-    for plot in coverage_plots/*${SUFFIX}; do
-        echo "<iframe src='${plot}' style='border:none;' width='100%' height='1000'>" >> ${BASENAME}${SUFFIX}
-        echo "</iframe>" >> ${BASENAME}${SUFFIX}
-    done
-    # Add the end
-    echo "</body>" >> ${BASENAME}${SUFFIX}
-    echo "</html>" >> ${BASENAME}${SUFFIX}
-done
+
+# # Ignore this
+# SUFFIXARRAY=( "_sample-normalised_coverage.html" "_amplicon-normalised_coverage.html" "_all.html" )
+
+# for SUFFIX in ${SUFFIXARRAY[@]}; do
+#     echo ${BASENAME}${SUFFIX}
+#     # Top bit of the html
+#     echo "<!DOCTYPE html>" > ${BASENAME}${SUFFIX}
+#     echo "<html>" >> ${BASENAME}${SUFFIX}
+#     echo "<body>" >> ${BASENAME}${SUFFIX}
+#     # Add iframes for each plot
+#     for plot in coverage_plots/*${SUFFIX}; do
+#         echo "<iframe src='${plot}' style='border:none;' width='100%' height='1000'>" >> ${BASENAME}${SUFFIX}
+#         echo "</iframe>" >> ${BASENAME}${SUFFIX}
+#     done
+#     # Add the end
+#     echo "</body>" >> ${BASENAME}${SUFFIX}
+#     echo "</html>" >> ${BASENAME}${SUFFIX}
+# done
--- plot_amplicon_coverage.R
+++ plot_amplicon_coverage.R
@@ -11,8 +11,8 @@ source("/Users/Jared/Documents/Code/amplicon-coverage-plots/amplicon_coverage_fu
 
 # # Manual input file paths
 # inputpath <- "/Users/Jared/Documents/Project_Results_and_Data/BSTP/run1-7_amplicon_coverage_plotting/run1-7_amplicon_metrics_clean_190218.txt"
-inputpath <- "/Users/Jared/Documents/Jobs/CONFIRM_PROSTRAP_test_plotting/amplicon_metrics_files/CNFRM-column_S3_L001.amplicon-metrics.txt"
-# inputpath <- "/Users/Jared/Documents/Jobs/CONFIRM_PROSTRAP_test_plotting/amplicon_metrics_files/PSTP-column_S2_L001.amplicon-metrics.txt"
+# inputpath <- "/Users/Jared/Documents/Jobs/CONFIRM_PROSTRAP_test_plotting/amplicon_metrics_files/CNFRM-column_S3_L001.amplicon-metrics.txt"
+inputpath <- "/Users/Jared/Documents/Jobs/CONFIRM_PROSTRAP_test_plotting/amplicon_metrics_files/PSTP-column_S2_L001.amplicon-metrics.txt"
 
 # Reading input
 amplicon_metrics <- read_delim(inputpath, 
@@ -27,14 +27,19 @@ plotdata <- amplicon_metrics[,c("amplicon_f", "sample", "reads")]
 # Creating main matrix
 plotdata_all <- to_heatmap_matrix(plotdata)
 
+
 # # Plotting single samples
 # Clustering function throws a fit with a single column dataframe (understandably) expanding the df to avoid this
 plotdata_all_expanded <- cbind(plotdata_all, plotdata_all)
 colnames(plotdata_all_expanded)[2] <- c("X")
-
 # Plotting (note, amplicon_norm will not work with a single sample duplicated)
-create_plots(plotdata_all_expanded, "CONFIRM", plot_type = "sample_norm")
-
+create_plots(plotdata_all_expanded, "PROSTRAP", plot_type = "depth")
+gene_names <- unique(gsub("_.+_.+", "", plotdata$amplicon_f))
+for (gene in gene_names) {
+  print(gene)
+  gene_subset <- data.matrix(plotdata_all_expanded[grep(gene, row.names(plotdata_all)),])
+  create_plots(gene_subset, gene, singlegene = TRUE, plot_type = "depth")
+}
 
 
 # # # # # # # # Gene plots # # # # # # # # 
